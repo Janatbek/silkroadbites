3c0b7744c11d07344efefbc7fe89507ce6410eab
Janatbek Sharsheyev <janatbek@gmail.com>
2020-05-12 06:58:21 -0600

    prepare for herku

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-app.use(express.static(__dirname));
+const PORT = process.env.PORT || 5000;
+console.log(process.env)
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, 'index.html'))
-});
-
-app.listen(3000, console.log('app is listening'));
