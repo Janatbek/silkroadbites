48782710b035018e3ec46440082f419949994738
Janatbek Sharsheyev <janatbek@gmail.com>
2020-05-12 06:32:10 -0600

    change to index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 app.use(express.static(__dirname));
 
-app.get('/app', (req,res) => {
+app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 });
 
