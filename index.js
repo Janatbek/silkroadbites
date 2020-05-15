6f6deef02f7c29bde2afb2369e7b971bc9d3d554
Janatbek Sharsheyev <janatbek@gmail.com>
2020-05-15 03:01:40 -0600

    changed the menu

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-console.log(process.env)
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
+//////////////
