f14ecd2c6fdce7b79c4acbbcf14d80435af60aa3
Janatbek Sharsheyev <janatbek@gmail.com>
2020-05-19 21:26:02 -0600

    deleted some imgaes

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-//////////////
+const fs = require('fs');
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(req, res) {
+  console.log('index.html');
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.get('/order', (req, res) => {
+  console.log('order');
+  res.sendFile(path.join(__dirname + '/about.html'));
+  //
+});
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
